32f650a140ee1a958ead901c64f1c65e85e8e858
Chris Eskow <chriseskow@gmail.com>
2009-08-09 00:36:15 -0400

    Modify install script to use symlinks instead of copying files

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 
 for file in *; do
   if [ "$file" != "`basename $0`" ]; then
+    src="$PWD/$file"
     dest="$HOME/.$file"
-    echo "Installing $file to $dest"
-    cp "$file" "$dest"
+    if [ -d "$dest" -a ! -L "$dest" ]; then
+      echo "Warning: $dest is a non-symlinked directory, skipping $file"
+    else
+      ln -fsv "$src" "$dest"
+    fi
   fi
 done
